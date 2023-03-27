a6f70eb973eb768c26c44e1124828d8e6afa74df
macs1756 <kindrativmaksum312@gmail.com>
2023-03-27 18:44:40 +0300

    start

---
+++ js/app.js
@@ -0,0 +1,107 @@
+
+
+
+const btnBurger = document.querySelector(".header__burger");
+const burger = document.querySelector(".burger__open");
+btnBurger.addEventListener("click", item =>{
+		btnBurger.classList.toggle("close");
+});
+
+btnBurger.addEventListener("click", ()=>{
+		document.querySelector("body").classList.toggle("scrollNone");
+		burger.classList.toggle("burger__open-active");
+});
+
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", ()=>{
+	if(scrollY>10){
+		header.classList.add("header__scroll");
+	}else{
+		header.classList.remove("header__scroll");
+	}
+});
+
+const ALL_LINKS_BURGER = document.querySelectorAll('.burger__open li');
+
+ALL_LINKS_BURGER.forEach((link)=>{
+	link.addEventListener('click', ()=>{
+		burger.classList.toggle("burger__open-active");
+		btnBurger.classList.toggle("close");
+	});
+});
+
+
+////part right
+
+
+const imageComparisonSlider = document.querySelector('[data-component="image-comparison-slider"]')
+
+function setSliderstate(e, element) {
+  const sliderRange = element.querySelector('[data-image-comparison-range]');
+
+  if (e.type === 'input') {
+    sliderRange.classList.add('image-comparison__range--active');
+    return;
+  }
+
+  sliderRange.classList.remove('image-comparison__range--active');
+  element.removeEventListener('mousemove', moveSliderThumb);
+}
+
+function moveSliderThumb(e) {
+  const sliderRange = document.querySelector('[data-image-comparison-range]');
+  const thumb = document.querySelector('[data-image-comparison-thumb]');
+  let position = e.layerY - 20;
+
+  if (e.layerY <= sliderRange.offsetTop) {
+    position = -20;
+  }
+
+  if (e.layerY >= sliderRange.offsetHeight) {
+    position = sliderRange.offsetHeight - 20;
+  }
+
+  thumb.style.top = `${position}px`;
+}
+
+
+function createWidthRightText(){
+	let wLeft = document.querySelector('.image-comparison__image-wrapper');
+	let wRightText = document.querySelector('.image-comparison__caption--after');
+	
+	wRightText.style.width = `calc(100% - ${wLeft.offsetWidth + 25}px)`;
+}
+
+createWidthRightText();
+
+function moveSliderRange(e, element) {
+  const value = e.target.value;
+  const slider = element.querySelector('[data-image-comparison-slider]');
+  const imageWrapperOverlay = element.querySelector('[data-image-comparison-overlay]');
+
+  slider.style.left = `${value}%`;
+  imageWrapperOverlay.style.width = `${value}%`;
+
+  element.addEventListener('mousemove', moveSliderThumb);
+  setSliderstate(e, element);
+
+  createWidthRightText();
+}
+
+function init(element) {
+  const sliderRange = element.querySelector('[data-image-comparison-range]');
+
+  if ('ontouchstart' in window === false) {
+    sliderRange.addEventListener('mouseup', e => setSliderstate(e, element));
+    sliderRange.addEventListener('mousedown', moveSliderThumb);
+  }
+
+  sliderRange.addEventListener('input', e => moveSliderRange(e, element));
+  sliderRange.addEventListener('change', e => moveSliderRange(e, element));
+}
+
+init(imageComparisonSlider);
+
+
+
